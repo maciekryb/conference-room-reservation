3c8519b18cc95363df02f816c29495e86506f08c
Maciej Rybak <maciek.rybak95@gmail.com>
2025-03-09 16:55:48 +0100

    create  update migrations

--- migrations/Version20250309155221.php
+++ migrations/Version20250309155221.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250306210203 extends AbstractMigration
+final class Version20250309155221 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,15 @@ final class Version20250306210203 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-
+        $this->addSql('CREATE TABLE conference_room (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL, capacity INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE reservation (id SERIAL NOT NULL, start_time TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, end_time TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, reserved_by VARCHAR(255) NOT NULL, conference_room_id INT NOT NULL, PRIMARY KEY(id))');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP TABLE conference_room');
+        $this->addSql('DROP TABLE reservation');
     }
 }
